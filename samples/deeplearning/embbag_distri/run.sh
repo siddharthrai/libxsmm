14351cd6e859a643fc282527ede745e9fef5a0cb
egeorgan <evangelos.georganas@intel.com>
2022-10-14 09:26:46 -0700

    Script ammend

--- samples/deeplearning/embbag_distri/run.sh
+++ samples/deeplearning/embbag_distri/run.sh
@@ -5,10 +5,10 @@
 ITERS=1000
 S=1
 alpha=1.16
-TH=56
-n=2048
+TH=16
+n=8192
 e=256
-p=1
+p=5
 
 if [ "x$1" != "x" ] ; then ITERS=$1; fi
 if [ "x$2" != "x" ] ; then S=$2; fi
@@ -26,12 +26,14 @@ NUMACTL_ARGS=" "
 if [ "${TH}" -gt "32" ]; then
   for T in ${TH} 32 16 8 4 1; do
     for N in ${n} ; do
-      for E in ${e}; do
+      for E in 64 256; do
         for P in ${p}; do
           for M in 10000000 ; do
-            export OMP_NUM_THREADS=$T
-            echo "Running: $NUMACTL_ARGS $BIN_FN $ITERS $N $E $M $S $P $alpha with $T threads"
-            $NUMACTL_ARGS $BIN_FN $ITERS $N $E $M $S $P $alpha
+            for alpha in 0 1.16 1.7; do
+              export OMP_NUM_THREADS=$T
+              echo "Running: $NUMACTL_ARGS $BIN_FN $ITERS $N $E $M $S $P $alpha with $T threads"
+              $NUMACTL_ARGS $BIN_FN $ITERS $N $E $M $S $P $alpha
+            done
           done
         done
       done
@@ -40,12 +42,14 @@ if [ "${TH}" -gt "32" ]; then
 else
   for T in 16 8 4 1; do
     for N in ${n} ; do
-      for E in ${e}; do
+      for E in 64 256; do
         for P in ${p}; do
           for M in 10000000 ; do
-            export OMP_NUM_THREADS=$T
-            echo "Running: $NUMACTL_ARGS $BIN_FN $ITERS $N $E $M $S $P $alpha with $T threads"
-            $NUMACTL_ARGS $BIN_FN $ITERS $N $E $M $S $P $alpha
+            for alpha in 0 1.16 1.7; do
+              export OMP_NUM_THREADS=$T
+              echo "Running: $NUMACTL_ARGS $BIN_FN $ITERS $N $E $M $S $P $alpha with $T threads"
+              $NUMACTL_ARGS $BIN_FN $ITERS $N $E $M $S $P $alpha
+            done
           done
         done
       done
